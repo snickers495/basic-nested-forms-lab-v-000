af259ce219563a05f671058afbec72ca17157508
Lisa Lee <snickers495@gmail.com>
2017-12-04 17:45:32 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,15 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build
   end
 
   def create
   end
+
+  private 
+
+  def recipe_params 
+    params.require(:recipe).permit(:title, ingredients_attributes: [:name, :quantity])
+  end 
 end
